1e8e5aed47c7a22a6db72c8fad520fe1cafcdea7
Philippdam <91878449+Philippdam@users.noreply.github.com>
2021-10-05 13:49:48 +0200

    Update Chat mit Ada.py

--- Python Projekte/Chat mit Ada.py
+++ Python Projekte/Chat mit Ada.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 
-NameUser = input ("Hallo! Ich bin ein Chatbot.\nWenn du wissen willst,wie ich heiße, frage micht einfach.\nWie heißt du?\n")
+NameUser = input ("Hallo! Ich bin ein Chatbot.\nWenn du wissen willst,wie ich heiße, frage mich einfach.\nWie heißt du?\n")
 print ("Hallo", NameUser, "du heißt ja genau wie mein Haustier! So ein Zufall! hihi.\nWie alt bist du?\n")
 
 Alter = int(input())
 if Alter < 18:
-    print ("Du bist ja jung, du BABY!\n Magst du Menschen? Ja oder Nein?\n")
+    print ("Du bist ja jung, du BABY!\nIch bin schon ganze 1 Tag alt! Magst du Züge? Ja oder Nein?\n")
 if Alter >= 18:
-    print ("Du bist ja alt...Du alte Schachtel!\n Magst du Menschen?Ja oder Nein?\n")
-if input() ==  "ja"or input() == "yes" or input() == "jo":
-    print ("OMG. ICH AUCH! Poka ! Poka!\n  Das ist Russisch und heißt Bye! Bye!")
+    print ("Du bist ja alt...Ich bin schon 1 Tag alt!\n Magst du Züge? Ja oder Nein?\n")
+if input() == "ja" or input == "Ja" or input() == "yes" or input() == "jo":
+    print ("OH MEIN PROGRAMMIERGOTT!\nICH AUCH!\nBye! Bye!")
 else:
-    print ("OMG. ICH AUCH!\n. Poka ! Poka! Das ist Russisch und heißt Bye! Bye!")
+    print ("OH MEIN PROGRAMMIERGOTT!\nJetzt lösche ich alle deine Daten auf deinem PC. Bye! Bye!")
 
-if input() ==  "nein"or input() == "no" or input() == "ne":
-    print ("OMG. ICH AUCH!\n. Poka ! Poka! Das ist Russisch und heißt Bye! Bye!")
-else:
-    print ("OMG. ICH AUCH!\n. Poka ! Poka! Das ist Russisch und heißt Bye! Bye!")
-
-if input() ==  "Wie heißt du?"or input() == "Wie ist dein Name?" or input() == "What´s your name?":
-    print ("Ich heiße Ada, Bby!")
+if input() ==  "Wie heißt du?"or input() == "Wie ist dein Name?" or input() == "what's your name":
+    print ("Ich heiße Ada! Merke dir den Namen gut, BABY!")
 
